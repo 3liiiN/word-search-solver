a3a53c4f21e85d781e7a5e9818f6c33334b265e9
Caroline Liu <34634457+liucar@users.noreply.github.com>
2017-12-17 20:41:05 -0800

    First commit

--- puzzle_solver.cpp
+++ puzzle_solver.cpp
@@ -1,21 +1,7 @@
 /*
  * @author Caroline Liu
  * @date 11/24/17
- * @assignment HW 08
- * @course CSCI 61
  * @file puzzle_solver.cpp
- *
- * A program that solves word search puzzles of arbitrary dimensions
- * that are lowercase. Locates all words of length 6 or more that
- * appear horizontally, vertically, and diagonally, either forwards
- * or backwards (no wraparounds).
- *
- * Uses "words.txt" as dictionary
- * Uses "puzzle.txt" as puzzle
- *
- * Generates "answers.txt" to report found words alphabetically with
- * start and end positions
- *
  */
 
 #include <iostream>
